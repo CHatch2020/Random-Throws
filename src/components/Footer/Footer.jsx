00ccdf2c9d15d1f3c2c9ea47e6acaf2ec06bd001
Caleb Hatch <hatchcaleb@gmail.com>
2021-12-19 15:43:22 -0600

    got nav bar inserted into project

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Footer.css';
 
+import NavBar from '../NavBar/NavBar';
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; Prime Digital Academy</footer>;
+  return <NavBar />;
 }
 
 export default Footer;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+
+import HomeIcon from '@mui/icons-material/Home';
+import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import BackpackIcon from '@mui/icons-material/Backpack';
+
+import { styled } from '@mui/material/styles';
+
+const StyledBottomNav = styled(BottomNavigation)`
+    position: absolute;
+    bottom: 0;
+    background-color: rgb(8, 162, 251);
+`;
+
+export default function NavBar() {
+  const [value, setValue] = React.useState('home');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <StyledBottomNav sx={{ width: 400 }} value={value} onChange={handleChange}>
+      <BottomNavigationAction
+        label="Home"
+        value="home"
+        icon={<HomeIcon />}
+      />
+      <BottomNavigationAction
+        label="Play"
+        value="play"
+        icon={<PlayCircleFilledWhiteIcon />}
+      />
+      <BottomNavigationAction
+        label="Courses"
+        value="courses"
+        icon={<LocationOnIcon />}
+      />
+      <BottomNavigationAction 
+        label="Bags" 
+        value="bags" 
+        icon={<BackpackIcon />} />
+    </StyledBottomNav>
+  );
+}
