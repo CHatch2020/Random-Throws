b5e5a8a8250bd5edef6947df27740cba245317cb
Caleb Hatch <hatchcaleb@gmail.com>
2022-01-06 22:18:58 -0600

    cleaned up index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createTheme, ThemeProvider } from '@mui/material';
 
 import store from './redux/store';
 
 import App from './components/App/App';
 
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
-
-
 ReactDOM.render(
-  <ThemeProvider theme={darkTheme}>
   <Provider store={store}>
     <App />
-  </Provider>
-  </ThemeProvider>,
+  </Provider>,
   document.getElementById('react-root'),
 );
