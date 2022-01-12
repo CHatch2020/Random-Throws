4edbcce7959956360d43b4a18f7ada027862b73c
Caleb Hatch <hatchcaleb@gmail.com>
2022-01-12 10:33:28 -0600

    styled disc edit component

--- src/components/EditDisc/EditDisc.jsx
+++ src/components/EditDisc/EditDisc.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
+
+import "./EditDisc.css";
 
 function EditDisc() {
   const params = useParams();
@@ -11,7 +13,7 @@ function EditDisc() {
 
   console.log(params.disc_id);
 
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const [speed, setSpeed] = useState(0);
   const [glide, setGlide] = useState(0);
   const [turn, setTurn] = useState(0);
@@ -19,39 +21,39 @@ function EditDisc() {
   const [stability, setStability] = useState(0);
 
   useEffect(() => {
-    dispatch({ type: 'FETCH_ONE_DISC', payload: params.disc_id })
-  },[])
+    dispatch({ type: "FETCH_ONE_DISC", payload: params.disc_id });
+  }, []);
 
-  const discToEdit = useSelector(store => store.discToEdit);
+  const discToEdit = useSelector((store) => store.discToEdit);
 
   const handleNameChange = (e) => {
-      dispatch({ type: 'CHANGE_NAME', payload: e.target.value });
+    dispatch({ type: "CHANGE_NAME", payload: e.target.value });
   };
 
   const handleSpeedChange = (e) => {
-    dispatch({ type: 'CHANGE_SPEED', payload: e.target.value });
-};
+    dispatch({ type: "CHANGE_SPEED", payload: e.target.value });
+  };
 
-const handleGlideChange = (e) => {
-    dispatch({ type: 'CHANGE_GLIDE', payload: e.target.value });
-};
+  const handleGlideChange = (e) => {
+    dispatch({ type: "CHANGE_GLIDE", payload: e.target.value });
+  };
 
-const handleTurnChange = (e) => {
-    dispatch({ type: 'CHANGE_TURN', payload: e.target.value });
-};
+  const handleTurnChange = (e) => {
+    dispatch({ type: "CHANGE_TURN", payload: e.target.value });
+  };
 
-const handleFadeChange = (e) => {
-    dispatch({ type: 'CHANGE_FADE', payload: e.target.value });
-};
+  const handleFadeChange = (e) => {
+    dispatch({ type: "CHANGE_FADE", payload: e.target.value });
+  };
 
-const handleStabilityChange = (e) => {
-    dispatch({ type: 'CHANGE_STABILITY', payload: e.target.value });
-};
+  const handleStabilityChange = (e) => {
+    dispatch({ type: "CHANGE_STABILITY", payload: e.target.value });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch({ 
-      type: 'EDIT_DISC', 
+    dispatch({
+      type: "EDIT_DISC",
       payload: {
         disc_id: params.disc_id,
         disc_name: discToEdit.disc_name,
@@ -60,53 +62,64 @@ const handleStabilityChange = (e) => {
         turn: discToEdit.turn,
         fade: discToEdit.fade,
         stability: discToEdit.stability,
-        bag_id: params.id
-    }});
+        bag_id: params.id,
+      },
+    });
     history.push(`/bags/${params.id}`);
-  }
+  };
 
   return (
     <div>
-      <h2>Edit This Disc</h2>
+      <h2 className="title">Edit Your {discToEdit.disc_name}</h2>
       <form onSubmit={handleSubmit}>
         <input
+          className="text-input"
           type="text"
           placeholder="Disc Name"
-          value={discToEdit.disc_name || ''}
-            onChange={handleNameChange}
+          value={discToEdit.disc_name || ""}
+          onChange={handleNameChange}
         />
         <input
+          className="number-input"
           type="number"
           placeholder="Speed"
           value={discToEdit.speed}
-            onChange={handleSpeedChange}
+          onChange={handleSpeedChange}
         />
         <input
+          className="number-input"
           type="number"
           placeholder="Glide"
           value={discToEdit.glide}
-            onChange={handleGlideChange}
+          onChange={handleGlideChange}
         />
         <input
+          className="number-input"
           type="number"
           placeholder="Turn"
           value={discToEdit.turn}
-            onChange={handleTurnChange}
+          onChange={handleTurnChange}
         />
         <input
+          className="number-input"
           type="number"
           placeholder="Fade"
           value={discToEdit.fade}
-            onChange={handleFadeChange}
+          onChange={handleFadeChange}
         />
         <input
+          className="text-input"
           type="text"
           placeholder="Stability"
-          value={discToEdit.stability || ''}
-            onChange={handleStabilityChange}
+          value={discToEdit.stability || ""}
+          onChange={handleStabilityChange}
         />
-        <button>Update</button>
-        <button onClick={(e) => history.push(`/bags/${params.id}`)}>Cancel</button>
+        <div className="buttons">
+          <button className="edit-update">Update</button>
+          <button className="edit-cancel" onClick={(e) => history.push(`/bags/${params.id}`)}>
+            Cancel
+          </button>
+        </div>
       </form>
     </div>
   );
